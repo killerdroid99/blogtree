4561443c9d0a2f754a90d999344cf06f29f3dae9
Aniket Jena <ajempire09@gmail.com>
2025-03-26 10:12:01 +0530

    fix: Better date formatting and markdown rendering
    Markdown rendering is WIP also remove the default welcome page

--- resources/js/pages/post.tsx
+++ resources/js/pages/post.tsx
@@ -1,6 +1,7 @@
 import MainLayout from '@/layouts/MainLayout'
 import { Head, usePage } from '@inertiajs/react'
-import { useMemo } from 'react'
+import { format } from 'date-fns'
+import ReactMarkdown from 'react-markdown';
 
 interface Post {
     id: string
@@ -11,30 +12,17 @@ interface Post {
 }
 function Post() {
     const { post } = usePage<{ post: Post }>().props
-    const formattedDate = useMemo(() => {
-        return new Date(post.published_at).toLocaleDateString(undefined, {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-        });
-    }, [post.published_at]);
+    const formattedDate = format(new Date(post.published_at), 'MMM d, yyyy')
     return (
         <MainLayout>
-            <Head title={`Post | (${post.title})`}>
-                <link rel="preconnect" href="https://fonts.bunny.net" />
-                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-            </Head>
+            <Head title={`Post | ${post.title}`} />
             <h1 className="text-3xl font-bold my-4">{post.title}</h1>
 
             <div className="text-sm text-gray-500 mb-2">
                 Published by {post.author_name} on {formattedDate}
             </div>
 
-            <p className='text-lg leading-relaxed mt-5'>
-                {post.body}
-            </p>
+            <ReactMarkdown>{post.body}</ReactMarkdown>
         </MainLayout>
     )
 }
